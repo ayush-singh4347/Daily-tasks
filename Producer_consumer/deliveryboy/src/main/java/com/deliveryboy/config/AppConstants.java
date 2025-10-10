8909a6c053f1c645bbd3aaf14f3016e9e0489af6
ayush-singh4347 <ayush.singh@encora.com>
2025-10-10 10:28:57 +0530

    9th oct

---
+++ Producer_consumer/deliveryboy/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,7 @@
+package com.deliveryboy.config;
+
+public class AppConstants {
+
+	public static final String location_topic_name = "location-update-topic";
+	public static final int location = 0;
+}
